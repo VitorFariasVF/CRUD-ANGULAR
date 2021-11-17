58b38b0ece829975a3a5526936b2dc420898c9ab
VitorFariasVF <80009432+VitorFariasVF@users.noreply.github.com>
2021-11-16 22:18:35 -0300

    Service no angular + fiz

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,13 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ]; //pode ser declarado dentro do constructor
+  courses: Course[] = [];
   displayedColumns = ['name','category'];
 
-  constructor() {
+  //CoursesService: CoursesService;
+
+  constructor(private CoursesService: CoursesService) {
     // this.courses =[];
+    //this.CoursesService = new CoursesService();
+    this.courses = this.CoursesService.list();
   }
 
   ngOnInit(): void {
